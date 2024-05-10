53b0dbec866c76009efbcaec59b7a0219f1c19bc
Mahima Singh <mahima2454.be21@chitkara.edu.in>
2024-05-10 22:36:40 +0530

    .

--- api/app.js
+++ api/app.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-
 import postRoute from './routes/post.route.js';
 import authRoute from './routes/auth.route.js';
 import testRoute from './routes/test.route.js';
+import userRoute from './routes/user.route.js';
+
 
 const app = express();
 
@@ -20,9 +21,12 @@ const app = express();
 app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
-app.use('/api/posts', postRoute);
+
 app.use('/api/auth', authRoute);
+app.use('/api/users', userRoute);
+app.use('/api/posts', postRoute);
 app.use('/api/test', testRoute);
+
 
 app.listen(8800, () => {
   console.log('Server is running!');
